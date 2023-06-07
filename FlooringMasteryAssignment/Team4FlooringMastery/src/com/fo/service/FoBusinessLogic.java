37700af4ffc0c16a867701a8bea67da1c9b77011
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-06-07 17:09:37 +0100

    Add 3 tests and businessLogic interface

--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/service/FoBusinessLogic.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/service/FoBusinessLogic.java
@@ -1,5 +1,64 @@
 package com.fo.service;
 
+import com.fo.dto.Order;
+import com.fo.dto.Product;
+import com.fo.dto.Tax;
+
+import com.fo.utility.EntryNotFoundException;
+
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
 public interface FoBusinessLogic {
+
+	// Note: The private methods should be implemented as private
+	// methods within the implementing class to provide the necessary
+	// internal functionality (but they should not be part of the public
+	// contract defined by the interface):
+
+	// private
+	//void placeOrder(Order order);
+
+	public List<Product> getProducts();
+
+	public List<State> getStates();
+
+	// private
+	//static LinkedList<Order> getOrders();
+
+	LinkedList<Order> getAllOrdersForDate(LocalDate date) throws FileNotFoundException;
+
+	Order createOrder(LocalDate orderDate, String customerName, String state, String productType, BigDecimal area);
+
+	public boolean checkName(String name) throws InvalidInputException;
+
+	public boolean checkState(String state) throws EntryNotFoundException;
+
+	public boolean checkProductType(String productType) throws EntryNotFoundException;
+
+	public boolean checkArea(BigDecimal area) throws InvalidInputException;
+
+	public boolean checkDate(LocalDate date);
+
+	public Order calculateOrder(LocalDate orderDate, int orderNumber, String customerName, Tax tax, Product product,
+			BigDecimal area);
+
+	// private
+	//int getOrderNumber();
+
+	public Order getOrder(int orderNumber);
+
+	public LinkedList<Order> editOrder(int orderNumber, Order order);
+
+	public void removeOrder(Order order);
+
+	public void saveOrdersToAFile();
+
+	public void exportData();
+	
+	// Question: how about the data types (State, Product)?
 
 }
--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CalculateOrderTest.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CalculateOrderTest.java
@@ -26,9 +26,36 @@ class CalculateOrderTest {
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Test // CalculateOrder01
+	void CalculateOrder() {
+		// Example used: 3,Albert
+		// Einstein,KY,6.00,Carpet,217.00,2.25,2.10,488.25,455.70,56.64,1000.59
+		String orderNumber = "3";
+		String customerName = "Albert Einstein";
+		String stateCode = "KY";
+		BigDecimal taxRate = 6.00;
+		String productType = "Carpet";
+		BigDecimal area = 217.00;
+		BigDecimal costPerSquareFoot = 2.25;
+		BigDecimal laborCostPerSquareFoot = 2.10;
+
+		LinkedList<Order> result = calculateOrder(orderNumber, customerName, stateCode, orderNumber, taxRate,
+				productType, area, costPerSquareFoot, laborCostPerSquareFoot);
+
+		// Check the size of the list
+		assertEquals(1, result.size()); 
+
+		// Get the third order from the list
+		Order order = result.get(2); 
+
+		BigDecimal materialCost = (area.multiply(costPerSquareFoot));
+		BigDecimal laborCost = (area.multiply(laborCostPerSquareFoot));
+		BigDecimal tax = (materialCost.add(laborCost)).multiply(taxRate.divide(new BigDecimal(100)));																	// division
+
+		// Calculate the orderTotal
+		BigDecimal orderTotal = materialCost.add(laborCost).add(tax); 
+
+		assertEquals(new BigDecimal("1000.59"), orderTotal);
 	}
 
 }
