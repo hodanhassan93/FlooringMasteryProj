b41bb3a3152aa605e3d9344b3e5e4afa567ebd73
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-06-07 15:42:47 +0100

    Added test cases

--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CreateOrderTest.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CreateOrderTest.java
@@ -2,13 +2,21 @@ package com.fo.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.fo.dto.Order;
+import com.fo.dto.Product;
+import com.fo.dto.Tax;
+import com.fo.service.FoBusinessLogicImpl;
 
 class CreateOrderTest {
+	FoBusinessLogicImpl service;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -20,15 +28,32 @@ class CreateOrderTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		service = new FoBusinessLogicImpl();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		service = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void createOrderTest01() {
+		Order order = service.createOrder(LocalDate.of(2099, 12, 30), "Jack Brown", "Texas", "Carpet", new BigDecimal("150"));
+		
+		Tax texasTax = new Tax();
+		texasTax.setStateAbbreviation("TX");
+		texasTax.setStateName("Texas");
+		texasTax.setTaxRate(new BigDecimal("4.45"));
+		
+		Product product = new Product();
+		product.setCostPerSquareFoot(new BigDecimal("2.25"));
+		product.setLaborCostPerSquareFoot(new BigDecimal("2.10"));
+		product.setProductType("Carpet");
+		
+		
+		Order testOrder = new Order(LocalDate.of(2099, 12, 30), 1, "Jack Brown", texasTax, product, new BigDecimal("150"), new BigDecimal("337.5"), new BigDecimal("315"), new BigDecimal("2903.62"), new BigDecimal("3556.12"));
+		
+		assertEquals(true, order.equals(testOrder));
 	}
 
 }
--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/TaxReadObjectTest.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/TaxReadObjectTest.java
@@ -2,16 +2,23 @@ package com.fo.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.fo.dataaccess.FoTaxDataAccessImpl;
+import com.fo.dto.Tax;
 
 class TaxReadObjectTest {
+	
+	FoTaxDataAccessImpl taxDataAccess;
 
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	static void setUpBeforeClass() throws Exception { 
 	}
 
 	@AfterAll
@@ -20,15 +27,23 @@ class TaxReadObjectTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		taxDataAccess = new FoTaxDataAccessImpl();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		taxDataAccess = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void readObjectTest01() {
+		LinkedList<Tax> taxes = new taxDataAccess.readObject("Taxes");
+		assertEquals(true, (taxes.size()>0));
+	}
+	
+	@Test
+	void reaadObjectTest02() {
+		assertThrows(FileNotFoundException.class, taxDataAccess.readObject("foo"));
 	}
 
 }
