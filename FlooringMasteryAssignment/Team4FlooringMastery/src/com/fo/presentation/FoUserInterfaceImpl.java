c9ecd44857a2e998d6090e818350af2c55b60b15
Hodan Hassan <hodan.a.hassan93@gmail.com>
2023-06-08 16:02:29 +0100

    userinterfaceimpl

--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/presentation/FoUserInterfaceImpl.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/presentation/FoUserInterfaceImpl.java
@@ -1,6 +1,9 @@
 package com.fo.presentation;
 import com.fo.service.*;
+import java.time.LocalDate;
 import java.util.Scanner;
+import com.fo.utility.InvalidDateException;
+import com.fo.utility.InvalidInputException;
 
 public class FoUserInterfaceImpl implements FoUserInterface {
 	private FoBusinessLogic foBusinessLogic=new FoBusinessLogicImpl();
@@ -21,12 +24,76 @@ public class FoUserInterfaceImpl implements FoUserInterface {
 	public void performMenu(int choice)throws Exception {
 		Scanner scanner=new Scanner(System.in);
 	switch (choice) {
+	//display orders
 		case 1: 
-			System.out.println("Which date would you like the Orders for?");
-			foBusinessLogic.checkDate();
+			System.out.println("Which date would you like the orders for?");
+			LocalDate date = LocalDate.parse(scanner.nextLine());
+			if (foBusinessLogic.checkDate(date)==true) {
+				foBusinessLogic.getAllOrdersForDate(date);
+			}
+			else {
+				throw new InvalidDateException("You have entered an invalid date");
+				}
 			break;
+			
+			//add order
 		case 2:
-
+			
+			boolean test = false;
+			do {
+				System.out.println("Enter order date (YYYY-MM-DD)");
+				LocalDate date2 = LocalDate.parse(scanner.nextLine());
+				
+				try {
+					test = foBusinessLogic.checkDate(date2);
+				} catch (InvalidDateException ex) {
+					throw new InvalidDateException("Please enter a date.");
+				}
+				
+				if (test == false)
+					System.out.println("Please provide a future date.");
+			}while (test == false);
+			
+			test = false;
+			do {
+				System.out.println("Enter you name");
+				LocalDate date2 = LocalDate.parse(scanner.nextLine());
+				
+				try {
+					test = foBusinessLogic.checkDate(date2);
+				} catch (InvalidDateException ex) {
+					throw new InvalidDateException("Please enter a date.");
+				}
+				
+				if (test == false)
+					System.out.println("Please provide a future date.");
+			}while (test == false);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			if () {
+				System.out.println("Please enter your name");
+				String name = scanner.nextLine();
+				if(foBusinessLogic.checkName(name)==true) {
+					System.out.println("Please enter your state");
+					String state = scanner.nextLine();
+					if(foBusinessLogic.checkState(state)==true) {
+						
+					}
+				}
+				else {
+					throw new InvalidInputException("You have entered an invalid input");	
+				}
+			}
+			else {
+				throw new InvalidDateException("You have entered an invalid date");
+			}
 			break;
 		case 3:
 
@@ -38,7 +105,7 @@ public class FoUserInterfaceImpl implements FoUserInterface {
            
 		case 5: 
 			foBusinessLogic.exportData();
-			System.out.println("You're data has been stored");
+			System.out.println("Your data has been stored");
             break;
             
 		case 6:
