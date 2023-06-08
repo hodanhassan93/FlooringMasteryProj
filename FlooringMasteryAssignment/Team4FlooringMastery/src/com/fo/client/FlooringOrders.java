7967d64b3720b7b4dcd65422d0511a836ef6f7f7
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-06-08 11:22:37 +0100

    Add CalculateOrderTest and FlooringOrders (main)

--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/client/FlooringOrders.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/client/FlooringOrders.java
@@ -1,5 +1,22 @@
 package com.fo.client;
 
+import java.util.Scanner;
+
+import com.vm.ui.VmUserInterface;
+import com.vm.ui.VmUserInterfaceImpl;
+
 public class FlooringOrders {
+
+	public static void main(String[] args) {
+		FoUserInterface foUserInterface = new FoUserInterfaceImpl();
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			foUserInterface.showMenu();
+			System.out.println("What would you like to do:");
+			int choice = scanner.nextDouble();
+			foUserInterface.performMenu(choice);
+		}
+	}
 
 }
--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CalculateOrderTest.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/test/CalculateOrderTest.java
@@ -28,34 +28,17 @@ class CalculateOrderTest {
 
 	@Test // CalculateOrder01
 	void CalculateOrder() {
-		// Example used: 3,Albert
-		// Einstein,KY,6.00,Carpet,217.00,2.25,2.10,488.25,455.70,56.64,1000.59
-		String orderNumber = "3";
-		String customerName = "Albert Einstein";
-		String stateCode = "KY";
 		BigDecimal taxRate = 6.00;
-		String productType = "Carpet";
 		BigDecimal area = 217.00;
 		BigDecimal costPerSquareFoot = 2.25;
 		BigDecimal laborCostPerSquareFoot = 2.10;
 
-		LinkedList<Order> result = calculateOrder(orderNumber, customerName, stateCode, orderNumber, taxRate,
-				productType, area, costPerSquareFoot, laborCostPerSquareFoot);
+		Order order = calculateOrder(taxRate, area, costPerSquareFoot, laborCostPerSquareFoot);
 
-		// Check the size of the list
-		assertEquals(1, result.size()); 
+		FoBusinessLogicImpl fobusinessLogicImpl = new FoBusinessLogicImpl();
 
-		// Get the third order from the list
-		Order order = result.get(2); 
-
-		BigDecimal materialCost = (area.multiply(costPerSquareFoot));
-		BigDecimal laborCost = (area.multiply(laborCostPerSquareFoot));
-		BigDecimal tax = (materialCost.add(laborCost)).multiply(taxRate.divide(new BigDecimal(100)));																	// division
-
-		// Calculate the orderTotal
-		BigDecimal orderTotal = materialCost.add(laborCost).add(tax); 
-
-		assertEquals(new BigDecimal("1000.59"), orderTotal);
+		BigDecimal expectedResult = new BigDecimal("1000.59");
+		
+		assertTrue(expectedResult == order.getTotal);
 	}
-
 }
