b9ee6f582f9a82adcfa8e6cd18e07e9ec85a4031
catalina-kalman <118086125+catalina-kalman@users.noreply.github.com>
2023-06-09 15:30:40 +0100

    Update FoUserInterface.java

--- FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/presentation/FoUserInterface.java
+++ FlooringMasteryAssignment/Team4FlooringMastery/src/com/fo/presentation/FoUserInterface.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public interface FoUserInterface {
 	
-	public void performMenu(String choice) throws FileNotFoundException, Exception;
+	public void performMenu(String choice);
 	public void showMenu();
 
 }
